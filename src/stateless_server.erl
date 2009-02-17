462be1526c67a5ceca32b18ca39ca3ca5adc9433
Nick Gerakines <nick@gerakines.net>
2009-02-17 12:07:05 -0800

    moving from proc_lib:start to proc_lib:start_link.

--- src/stateless_server.erl
+++ src/stateless_server.erl
@@ -1,17 +1,45 @@
+%% Copyright (c) 2009 Nick Gerakines <nick@gerakines.net>
+%%
+%% Permission is hereby granted, free of charge, to any person
+%% obtaining a copy of this software and associated documentation
+%% files (the "Software"), to deal in the Software without
+%% restriction, including without limitation the rights to use,
+%% copy, modify, merge, publish, distribute, sublicense, and/or sell
+%% copies of the Software, and to permit persons to whom the
+%% Software is furnished to do so, subject to the following
+%% conditions:
+%%
+%% The above copyright notice and this permission notice shall be
+%% included in all copies or substantial portions of the Software.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+%% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+%% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+%% HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+%% WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+%% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+%% OTHER DEALINGS IN THE SOFTWARE.
+%% @doc A small, no frills gen_server clone. 
+%% @author Nick Gerakines <nick@gerakines.net>
 -module(stateless_server).
 
--export([start/1, start_server/1, server_loop/1, behaviour_info/1, call/2]).
+-export([start/1, start_server/2, server_loop/1, behaviour_info/1, call/2]).
 
-behaviour_info(callbacks) -> [{init, 1}, {handle, 2}, {terminate, 1}];
+behaviour_info(callbacks) -> [{init, 1}, {handle, 2}];
 behaviour_info(_) -> undefined.
 
+%% @spec start(Module) -> pid()
+%%       Module = atom()
+%% @doc Starts the statless_server process with a given callback module.
 start(Module) ->
-    proc_lib:start(stateless_server, start_server,[Module]).
+    proc_lib:start_link(stateless_server, start_server,[self(), Module]).
 
-start_server(Module) ->
+%% @private
+start_server(Parent, Module) ->
     try Module:init(Module) of
          ok ->
-            proc_lib:init_ack(ok),
+             proc_lib:init_ack(Parent, {ok, self()}),
             error_logger:info_report([stateless_server, {server_start, Module}, init_ok]),
             stateless_server:server_loop(Module);
         Response ->
@@ -21,6 +49,7 @@ start_server(Module) ->
             error_logger:error_report([stateless_server, {server_start, init_nok}, {Ma, Mi}])
     end.
 
+%% @private
 server_loop(Module) ->
     receive
         {From, Message} ->
@@ -41,6 +70,11 @@ server_loop(Module) ->
     end,
     stateless_server:server_loop(Module).
 
+%% @spec call(To, Message) -> Response
+%%       To = atom | pid()
+%%       Message = any()
+%%       Response = noreply | any()
+%% @doc Sends a message to a stateless_server process.
 call(To, Message) ->
     To ! {self(), Message},
     receive
