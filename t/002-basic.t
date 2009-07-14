26adbd5486bf7a76dc73ef363a4d601b40913c5a
Nick Gerakines <nick@gerakines.net>
2009-07-13 19:52:31 -0700

    Adding test cases to assert that exceptions and timeouts are handled properly

--- t/002-basic.t
+++ t/002-basic.t
@@ -3,8 +3,10 @@
 %%! -pa ./ebin -sasl sasl_error_logger false
 
 main(_) ->
-    etap:plan(2),
+    etap:plan(3),
     {ok, ServerPid} = stateless_server:start(basic_server),
     etap:is(basic_server:foo(ServerPid), bar, "basic i/o"),
     etap:is(basic_server:good(ServerPid), evil, "hyjacked message"),
+    etap:is(catch(basic_server:thrown(ServerPid)), {'EXIT', wutt}, "Exception caught"),
+    etap:is(catch(basic_server:sleep(ServerPid)), {'EXIT', timeout}, "Exception caught (timeout)"),
     etap:end_tests().
--- t/basic_server.erl
+++ t/basic_server.erl
@@ -1,13 +1,21 @@
 -module(basic_server).
 -behavior(stateless_server).
 
--export([init/1, handle/2, foo/1, good/1]).
+-export([init/1, handle/2, foo/1, good/1, thrown/1, sleep/1]).
 
 init(_) ->
     ok.
 
 handle(_From, {foo}) ->
     {reply, bar};
+
+handle(_From, {thrown}) ->
+    exit(wutt),
+    {reply, ok};
+
+handle(_From, {sleep}) ->
+    timer:sleep(9000),
+    {reply, ok};
 
 handle(From, {evil}) ->
     From ! {reply, good},
@@ -21,3 +29,9 @@ foo(Pid) ->
 
 good(Pid) ->
     stateless_server:call(Pid, {evil}).
+
+thrown(Pid) ->
+    stateless_server:call(Pid, {thrown}).
+
+sleep(Pid) ->
+    stateless_server:call(Pid, {sleep}).
